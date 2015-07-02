67cb55502931a028397364d5c5142f4b9ab69886
joelworsham <joelworsham@gmail.com>
2015-07-02 10:08:30 -0400

    Resolves #19

--- woocommerce/overrides.php
+++ woocommerce/overrides.php
@@ -14,6 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 add_filter( 'woocommerce_continue_shopping_redirect', '_kniche_modify_cart_success_link' );
 add_action( 'woocommerce_after_shop_loop_item_title', '_kniche_product_subtitle' );
 add_action( 'woocommerce_after_shop_loop_item_title', '_kniche_product_subtitle' );
+add_filter('loop_shop_columns', '_kniche_loop_columns');
 
 function _kniche_modify_cart_success_link() {
 	return get_permalink( wc_get_page_id( 'shop' ) );
@@ -21,11 +22,15 @@ function _kniche_modify_cart_success_link() {
 
 function _kniche_product_subtitle() {
 
-//	global $post;
+	//	global $post;
 
 	if ( $field = get_field( 'sub_title' ) ) : ?>
 		<p class="product-subtitle">
 			<?php echo $field; ?>
 		</p>
 	<?php endif;
+}
+
+function _kniche_loop_columns() {
+	return 3; // 3 products per row
 }
