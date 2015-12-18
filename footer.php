82bcce5042680a7661874f8e6e779e034bacf780
Eric Defore <eric.defore@gmail.com>
2015-12-17 19:39:20 -0500

    Added Updated Universal Google Analytics Script. Closes #80

--- footer.php
+++ footer.php
@@ -37,5 +37,18 @@ $privacy_policy_page = get_page_by_path( 'privacy-policy' );
 
 <?php wp_footer(); ?>
 
+<script type = "text/javascript">
+
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-71404004-1', 'auto');
+    ga('require', 'linkid');
+    ga('send', 'pageview');
+
+</script>
+
 </body>
 </html>
